fa50b54b65e43f69c1bdea85a9d087b6e0ef6375
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-27 04:09:26 +0500

    FEAT: Working on...
    - ElectionListAdapter

--- app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -1,23 +1,109 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ItemElectionBinding
 import com.example.android.politicalpreparedness.network.models.Election
 
 class ElectionListAdapter(private val clickListener: ElectionListener) :
-    ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
+    ListAdapter<Election, ElectionListAdapter.ElectionViewHolder>(ElectionDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
         return ElectionViewHolder.from(parent)
     }
 
-    //TODO: Bind ViewHolder
+    //DONE: Bind ViewHolder
+    override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
+        holder.bind(clickListener, getItem(position))
+    }
 
-    //TODO: Add companion object to inflate ViewHolder (from)
+
+    //DONE: Create ElectionViewHolder
+    class ElectionViewHolder(var binding: ItemElectionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        //DONE: Add companion object to inflate ViewHolder (from)
+        companion object {
+            fun from(parent: ViewGroup): ElectionViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemElectionBinding.inflate(layoutInflater, parent, false)
+                return ElectionViewHolder(binding)
+            }
+        }
+
+        fun bind(listener: ElectionListener, election: Election) {
+            binding.election = election
+            binding.clickListener = listener
+            binding.executePendingBindings()
+        }
+
+    }
+
+    //DONE: Create ElectionDiffCallback
+    class ElectionDiffCallback : DiffUtil.ItemCallback<Election>() {
+        /**
+         * Called to check whether two objects represent the same item.
+         *
+         *
+         * For example, if your items have unique ids, this method should check their id equality.
+         *
+         *
+         * Note: `null` items in the list are assumed to be the same as another `null`
+         * item and are assumed to not be the same as a non-`null` item. This callback will
+         * not be invoked for either of those cases.
+         *
+         * @param oldItem The item in the old list.
+         * @param newItem The item in the new list.
+         * @return True if the two items represent the same object or false if they are different.
+         *
+         * @see Callback.areItemsTheSame
+         */
+        override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        /**
+         * Called to check whether two items have the same data.
+         *
+         *
+         * This information is used to detect if the contents of an item have changed.
+         *
+         *
+         * This method to check equality instead of [Object.equals] so that you can
+         * change its behavior depending on your UI.
+         *
+         *
+         * For example, if you are using DiffUtil with a
+         * [RecyclerView.Adapter], you should
+         * return whether the items' visual representations are the same.
+         *
+         *
+         * This method is called only if [.areItemsTheSame] returns `true` for
+         * these items.
+         *
+         *
+         * Note: Two `null` items are assumed to represent the same contents. This callback
+         * will not be invoked for this case.
+         *
+         * @param oldItem The item in the old list.
+         * @param newItem The item in the new list.
+         * @return True if the contents of the items are the same or false if they are different.
+         *
+         * @see Callback.areContentsTheSame
+         */
+        override fun areContentsTheSame(oldItem: Election, newItem: Election): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    //DONE: Create ElectionListener
+    class ElectionListener(val clickListener: (election: Election) -> Unit) {
+        fun onClick(election: Election) = clickListener(election)
+    }
+
+
 }
-
-//TODO: Create ElectionViewHolder
-
-//TODO: Create ElectionDiffCallback
-
-//TODO: Create ElectionListener
