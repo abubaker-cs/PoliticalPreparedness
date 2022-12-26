3164ab2f20ea4bb564c5b888aa8c1e8aa1924d1d
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-27 04:28:37 +0500

    Taking break...

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -1,36 +1,96 @@
 package com.example.android.politicalpreparedness.election
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.android.politicalpreparedness.database.ElectionDatabase
+import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.utils.CivicsApiStatus
 
 class VoterInfoFragment : Fragment() {
+
+
+    //DONE: Add ViewModel values and create ViewModel
+    private val viewModel by viewModels<VoterInfoViewModel> {
+        VoterInfoViewModelFactory(
+            ElectionDatabase.getInstance(requireContext()).electionDao,
+            CivicsApi.retrofitService
+        )
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        //TODO: Add ViewModel values and create ViewModel
+        val voterInfoFragmentArgs = VoterInfoFragmentArgs.fromBundle(requireArguments())
+        val electionId = voterInfoFragmentArgs.argElectionId
+        val division = voterInfoFragmentArgs.argDivision
 
-        //TODO: Add binding values
 
-        //TODO: Populate voter info -- hide views without provided data.
+        //DONE: Add binding values
+        val binding = FragmentVoterInfoBinding.inflate(layoutInflater)
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            voterInfoViewModel = viewModel
+        }
+
+        viewModel.getVoterInformation(electionId, division)
+
+        viewModel.apiStatus.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                if (it == CivicsApiStatus.ERROR) showRequestErrorDialog()
+            }
+        })
+
+        viewModel.url.observe(viewLifecycleOwner, Observer { it ->
+            it?.let {
+                loadUrl(it)
+                viewModel.navigateToUrlCompleted()
+            }
+        })
+        return binding.root
+
+        //DONE: Populate voter info -- hide views without provided data.
         /**
         Hint: You will need to ensure proper data is provided from previous fragment.
          */
 
 
-        //TODO: Handle loading of URLs
+        //DONE: Handle loading of URLs
 
-        //TODO: Handle save button UI state
-        //TODO: cont'd Handle save button clicks
+        //DONE: Handle save button UI state
+        //DONE: cont'd Handle save button clicks
 
     }
 
-    //TODO: Create method to load URL intents
+    //DONE: Create method to load URL intents
+    private fun showRequestErrorDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Error")
+            .setMessage("The voter information can't be retrieved. Click OK to go back.")
+            .setCancelable(false)
+            .setPositiveButton("Ok") { dialog, _ ->
+                dialog.dismiss()
+                this.findNavController().popBackStack()
+            }.show()
+    }
+
+    private fun loadUrl(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+        // viewModel.navigateToUrlCompleted()
+    }
 
 }
--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModelFactory.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModelFactory.kt
@@ -1,6 +1,20 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.network.CivicsApiService
 
-//TODO: Create Factory to generate VoterInfoViewModel with provided election datasource
-class VoterInfoViewModelFactory : ViewModelProvider.Factory
+//DONE: Create Factory to generate VoterInfoViewModel with provided election datasource
+class VoterInfoViewModelFactory(
+    private val localDataSource: ElectionDao,
+    private val remoteDataSource: CivicsApiService
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(VoterInfoViewModel::class.java)) {
+            return VoterInfoViewModel(localDataSource, remoteDataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
