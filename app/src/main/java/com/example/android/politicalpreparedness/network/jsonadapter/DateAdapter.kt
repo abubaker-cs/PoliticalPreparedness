ad792a702604c9a8e763c9f26b2eb877ad86def0
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-29 04:53:03 +0500

    feat: adding comments
    - network/*.kt

--- app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/DateAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/DateAdapter.kt
@@ -5,9 +5,13 @@ import com.squareup.moshi.ToJson
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * DateAdapter is a custom adapter that is used to convert a date string to a Date object as
+ * Moshi.Builder().add(DateAdapter()) in CivicsApiService.kt
+ */
 class DateAdapter {
 
-    //
+    // SimpleDateFormat is a concrete class for formatting and parsing dates in a locale-sensitive manner.
     private val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
 
     @FromJson
--- app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/ElectionAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/ElectionAdapter.kt
@@ -4,18 +4,39 @@ import com.example.android.politicalpreparedness.network.models.Division
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 
+/**
+ * ElectionAdapter is a custom adapter that is used to convert a division object to a String as
+ * Moshi.Builder().add(ElectionAdapter()) in CivicsApiService.kt
+ */
 class ElectionAdapter {
+
+    // divisionFromJson() is used to convert a division object to a String
     @FromJson
     fun divisionFromJson(ocdDivisionId: String): Division {
+
+        // countryDelimiter
         val countryDelimiter = "country:"
+
+        // stateDelimiter
         val stateDelimiter = "state:"
+
+        // country
         val country = ocdDivisionId.substringAfter(countryDelimiter, "")
             .substringBefore("/")
+
+        // state
         val state = ocdDivisionId.substringAfter(stateDelimiter, "")
             .substringBefore("/")
-        return Division(ocdDivisionId, country, state)
+
+        // Return a Division object
+        return Division(
+            ocdDivisionId,
+            country,
+            state
+        )
     }
 
+    // divisionToJson() is used to convert a String to a division object
     @ToJson
     fun divisionToJson(division: Division): String {
         return division.id
