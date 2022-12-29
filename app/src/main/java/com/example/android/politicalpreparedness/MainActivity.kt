c56a95afc129a9914ba40f0077ad5a7231a29821
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-29 11:55:47 +0500

    fixed:
    - Moved the hardcoded "Party" value from item_representative.xml to @res/values/strings.xml file.
    
    - I am now using DataBindingUtils to inflate @layout/activity_main.xml in MainActivity.kt
    
    - Added remaining contentDescription to the @+id/wwwIcon in item_representative.xml

--- app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.android.politicalpreparedness
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android.politicalpreparedness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     // onCreate() method is called when activity is first created
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +15,11 @@ class MainActivity : AppCompatActivity() {
         // Call super class onCreate to complete the creation of activity like the view hierarchy
         super.onCreate(savedInstanceState)
 
-        // Set the activity content to @res/layout/activity_main.xml
-        setContentView(R.layout.activity_main)
+        // Inflate Layout: @layout/activity_main.xml
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Activity
+        binding.lifecycleOwner = this
 
     }
 
