4f0e091d35e104cb3ffeb82477b1226ab72abcee
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-29 06:53:38 +0500

    feat: adding comments
    - ElectionsViewModel.kt
    - VoterInfoViewModel.kt

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -18,64 +18,114 @@ class ElectionsViewModel(
     private val apiService: CivicsApiService
 ) : ViewModel() {
 
+    // API Status
     private val _apiStatus = MutableLiveData<CivicsApiStatus>()
     val apiStatus: LiveData<CivicsApiStatus>
         get() = _apiStatus
 
+    // IsDbLoading
     private val _isDbLoading = MutableLiveData<Boolean>()
     val isDbLoading: LiveData<Boolean>
         get() = _isDbLoading
 
+    // Upcoming Elections
     private val _upComingElections = MutableLiveData<List<Election>>()
     val upcomingElections: LiveData<List<Election>>
         get() = _upComingElections
 
+    // Saved Elections
     private val _savedElections = MutableLiveData<List<Election>>()
     val savedElections: LiveData<List<Election>>
         get() = _savedElections
 
+    // Navigate to Voter Info
     private val _navigateToVoterInfo = MutableLiveData<Election>()
     val navigateToVoterInfo: LiveData<Election>
         get() = _navigateToVoterInfo
 
+    /**
+     * Initial setup
+     */
     init {
+
+        // Get: Upcoming Elections
         getUpcomingElectionsFromAPI()
+
+        // Get: Saved Elections
         getSavedElectionsFromDatabase()
+
     }
 
+    /**
+     * Get upcoming elections from API
+     */
     private fun getUpcomingElectionsFromAPI() {
+
+        // API Status: Loading
         _apiStatus.value = CivicsApiStatus.LOADING
+
+        // Coroutine to get the elections from the API
         viewModelScope.launch {
+
             try {
+
+                //  Get: Upcoming Elections
                 val response = apiService.getElections()
+
+                // API Status: Done
                 _apiStatus.value = CivicsApiStatus.DONE
+
+                // Set: Upcoming Elections to the response.elections
                 _upComingElections.value = response.elections
 
             } catch (e: Exception) {
-                Timber.e("Error: %s", e.localizedMessage)
+
+                // API Status: Error
                 _apiStatus.value = CivicsApiStatus.ERROR
+
+                // Log: Error
+                Timber.e("Error: %s", e.localizedMessage)
+
+                // Set: Upcoming Elections to empty list
                 _upComingElections.value = ArrayList()
+
             }
         }
     }
 
+    /**
+     * Get saved elections from database
+     */
     private fun getSavedElectionsFromDatabase() {
+
+        // IsDbLoading: True
         _isDbLoading.value = true
+
+        // Coroutine to get the elections from the database
         viewModelScope.launch {
+
+            // Get: Saved Elections
             _savedElections.value = database.getElections()
+
+            // IsDbLoading: False
             _isDbLoading.value = false
+
         }
     }
 
+    // Refresh: Saved Elections from the local database
+    fun refresh() {
+        getSavedElectionsFromDatabase()
+    }
+
+    // Display: Voter Info
     fun displayVoterInfo(election: Election) {
         _navigateToVoterInfo.value = election
     }
 
+    // Display: Voter Info Completed
     fun displayVoterInfoComplete() {
         _navigateToVoterInfo.value = null
     }
 
-    fun refresh() {
-        getSavedElectionsFromDatabase()
-    }
 }
--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -20,85 +20,124 @@ class VoterInfoViewModel(
 ) : ViewModel() {
 
     //DONE: Add live data to hold voter info
+
+    // API Status
     private val _apiStatus = MutableLiveData<CivicsApiStatus>()
     val apiStatus: LiveData<CivicsApiStatus>
         get() = _apiStatus
 
+    //DONE: Add var and methods to populate voter info
+    //DONE: Add var to save and remove elections to local database
+
+    // Election
     private val _election = MutableLiveData<Election>()
     val election: LiveData<Election>
         get() = _election
 
+    // Administration Body
     private val _administrationBody = MutableLiveData<AdministrationBody>()
     val administrationBody: LiveData<AdministrationBody>
         get() = _administrationBody
 
+    //DONE: Add var and methods to support loading URLs
+
+    // URL
     private val _url = MutableLiveData<String>()
     val url: LiveData<String>
         get() = _url
 
+    // isElectionSaved (Boolean)
     private val _isElectionSaved = MutableLiveData<Boolean>()
     val isElectionSaved: LiveData<Boolean>
         get() = _isElectionSaved
 
+    /**
+     * getVoterInfo() - Get Voter Info from API
+     */
     fun getVoterInfo(electionId: Int, division: Division) {
 
         _apiStatus.value = CivicsApiStatus.LOADING
 
         viewModelScope.launch {
             try {
+
                 val savedElection: Election? = dataSource.getElection(electionId)
+
+                //DONE: Populate initial state of save button to reflect proper action based on election saved status
                 _isElectionSaved.value = savedElection != null
 
                 val address = "${division.state}, ${division.country}"
                 val voterInfoResponse = apiService.getVoterInfo(address, electionId)
 
-                Timber.d("voterInfoResponse, %s", voterInfoResponse)
                 _election.value = voterInfoResponse.election
 
-                Timber.d(
-                    "AdministrationBody: %s",
-                    voterInfoResponse.state?.first()?.electionAdministrationBody
-                )
                 _administrationBody.value =
                     voterInfoResponse.state?.first()?.electionAdministrationBody
 
                 _apiStatus.value = CivicsApiStatus.DONE
             } catch (e: Exception) {
-                Timber.e("Error when retrieving voter information")
+
+                // API Status: Error
                 _apiStatus.value = CivicsApiStatus.ERROR
+
+                // Log Error message
+                Timber.e("Error when retrieving voter information")
+
             }
         }
     }
 
+    /**
+     * navigateToUrl() - Navigate to URL
+     */
     fun navigateToUrl(url: String) {
+
+        // Update live data to navigate to selected URL
         _url.value = url
+
     }
 
+    /**
+     * doneNavigating() - Done navigating
+     */
     @SuppressLint("NullSafeMutableLiveData")
     fun navigateToUrlCompleted() {
+
+        // DONE: Set _url to null
         _url.value = null
+
     }
 
+    //DONE: Add var and methods to save and remove elections to local database
+    /**
+     * toggleElectionSaved() - Toggle election saved
+     */
     fun toggleFollowElection() {
+
+        // DONE: Toggle saved election
         viewModelScope.launch {
             _election.value?.let {
+
                 if (_isElectionSaved.value == true) {
+
+                    // Remove election from saved elections
                     dataSource.deleteElection(it)
+
+                    // Set isElectionSaved to false
                     _isElectionSaved.value = false
+
                 } else {
+
+                    // Add election to saved elections
                     dataSource.insertElection(it)
+
+                    // Set isElectionSaved to true
                     _isElectionSaved.value = true
+
                 }
             }
         }
     }
-
-    //DONE: Add var and methods to populate voter info
-
-    //DONE: Add var and methods to support loading URLs
-
-    //DONE: Add var and methods to save and remove elections to local database
-    //DONE: Populate initial state of save button to reflect proper action based on election saved status
 
     /**
      * Hint: The saved state can be accomplished in multiple ways. It is directly related to how elections are saved/removed from the database.
